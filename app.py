a3c3c082879b0ef415642f376e02478b5fddc4e8
ilmari21 <juuso.u67@gmail.com>
2025-02-13 15:49:25 +0200

    Added item classes

--- app.py
+++ app.py
@@ -33,7 +33,8 @@ def show_item(item_id):
     item = items.get_item(item_id)
     if not item:
         abort(404)
-    return render_template("show_item.html", item = item)
+    classes = items.get_classes(item_id)
+    return render_template("show_item.html", item = item, classes = classes)
 
 @app.route("/new_item")
 def new_item():
@@ -56,7 +57,12 @@ def create_item():
         abort(403)
     user_id = session["user_id"]
 
-    items.add_item(title, description, location, user_id)
+    classes = []
+    section = request.form["section"]
+    if section:
+        classes.append(("Osasto", section))
+
+    items.add_item(title, description, location, user_id, classes)
 
     return redirect("/")
 
--- items.py
+++ items.py
@@ -1,8 +1,15 @@
 import db
 
-def add_item(title, description, location, user_id):
+def add_item(title, description, location, user_id, classes):
     sql = """INSERT INTO items (title, description, location, user_id) VALUES (?, ?, ?, ?)"""
     db.execute(sql, [title, description, location, user_id])
+
+    item_id = db.last_insert_id()
+
+    sql = "INSERT INTO item_classes (item_id, title, value) VALUES (?, ?, ?)"
+    for title, value in classes:
+        db.execute(sql, [item_id, title, value])
+
 
 def get_items():
     sql = "SELECT id, title FROM items ORDER BY id DESC"
@@ -20,6 +27,10 @@ def get_item(item_id):
                    items.id = ?"""
     result = db.query(sql, [item_id])
     return result[0] if result else None
+
+def get_classes(item_id):
+    sql = "SELECT title, value FROM item_classes WHERE item_id = ?"
+    return db.query(sql, [item_id])
 
 def update_item(item_id, title, description, location):
     sql = """UPDATE items SET title = ?,

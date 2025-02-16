e71aaf55e8c5b4ebe14017c9eabef671c6dde96f
ilmari21 <juuso.u67@gmail.com>
2025-02-16 23:00:44 +0200

    Fixed dysfunctional item deletion

--- items.py
+++ items.py
@@ -78,6 +78,8 @@ def update_item(item_id, title, description, location, classes):
         db.execute(sql, [item_id, title, value])
 
 def delete_item(item_id):
+    sql = "DELETE FROM applications WHERE item_id = ?"
+    db.execute(sql, [item_id])
     sql = """DELETE FROM item_classes WHERE item_id = ?"""
     db.execute(sql, [item_id])
     sql = """DELETE FROM items WHERE id = ?"""
